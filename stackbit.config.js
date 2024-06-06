430668630b9ba8c19afb33b1e73230bb79e23f6f
jrbarazone <jrlezama@barazone.com.mx>
2024-06-05 22:03:11 -0600

    Create stackbit.config.js

---
+++ stackbit.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    stackbitVersion: '~0.5.0',
+    ssgName: 'next',
+    nodeVersion: '12',
+    modelsSource: {
+        type: 'git',
+        repository: 'https://github.com/YOUR_GITHUB_USERNAME/YOUR_PROJECT_REPOSITORY'
+    }
+};
